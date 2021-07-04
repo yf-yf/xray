e3c798774020a889cc5dbeecf9e0236bdfc89a7f
yf-yf <65352022+yf-yf@users.noreply.github.com>
2021-07-04 13:22:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -193,6 +193,7 @@ EOF
     systemctl restart nginx.service
     green "$(date +"%Y-%m-%d %H:%M:%S") - 使用acme.sh申请https证书."
     curl https://get.acme.sh | sh
+    acme.sh --set-default-ca --server  letsencrypt
     ~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/
     if test -s /root/.acme.sh/$your_domain/fullchain.cer; then
         green "$(date +"%Y-%m-%d %H:%M:%S") - 申请https证书成功."
